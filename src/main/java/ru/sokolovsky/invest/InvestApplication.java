30105808ad8134195c2fc2bcb5544bc16c34557a
sokol-forbes <sasha.sokolovsky01@gmail.com>
2022-04-29 12:43:53 +0300

    init commit

---
+++ src/main/java/ru/sokolovsky/invest/InvestApplication.java
@@ -0,0 +1,14 @@
+package ru.sokolovsky.invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
